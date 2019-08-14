217ed812e6b9da72805ce69c40b693c43c5ad3dd
kogm <cnxxjs2538@163.com>
2019-08-14 19:12:40 +0800

    x

--- packages/mitod-react-native-hooks/src/animations/useAnim.tsx
+++
@@ -1,7 +0,0 @@
-import { useRef } from 'react';
-import { Animated } from 'react-native';
-
-export default function useAnimation(initialValue: number): Animated.Value {
-  const ref = useRef(new Animated.Value(initialValue));
-  return ref.current;
-}
