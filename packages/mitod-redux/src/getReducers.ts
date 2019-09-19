af09902ad343a6246e6e36eac84785ce68e92f85
kogm <cnxxjs2538@163.com>
2019-09-19 17:59:54 +0800

    添加提示

--- packages/mitod-redux/src/getReducers.ts
+++ packages/mitod-redux/src/getReducers.ts
@@ -10,6 +10,10 @@ export default function getReducers(
   return (state: ModelState = initState, action: any): ModelState => {
     const path = action.type.match(/^(\S+\.reducers)\/(\S+)$/);
     if (path && path[1] === `${namespace}.reducers`) {
+      if (!reducers[path[2]]) {
+        console.warn(`不存在该 ${namespace}.reducers/${path[2]}`);
+      }
+
       const actor = reducers[path[2]](state, action);
       return (actor as unknown) as ModelState;
     }
