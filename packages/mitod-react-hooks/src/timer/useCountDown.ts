0ed16f5692aad4c6bb312ecf380cf5f8efaad3da
kogm <cnxxjs2538@163.com>
2019-09-22 11:47:22 +0800

    fix useCountDown

--- packages/mitod-react-hooks/src/timer/useCountDown.ts
+++ packages/mitod-react-hooks/src/timer/useCountDown.ts
@@ -5,32 +5,14 @@ import useInterval from './useInterval';
 const defaultDelay = 1000;
 const defaultCountTime = 30;
 
-type CountDownType = [
-  number,
-  boolean,
-  React.Dispatch<React.SetStateAction<boolean>>
-];
-
-const useCountDown = (
-  fn?: () => void,
-  countTime = defaultCountTime,
-): CountDownType => {
-  const [start, setStart] = useState(false); // 开启倒计时
+const useCountDown = (countTime: number= defaultCountTime) => {
+  const [isStart, setStart] = useState(false); // 开启倒计时
   const [countDown, setCountDown] = useState(countTime); // 倒计时
 
-  useInterval(
-    () => {
-      if (countDown === 0) {
-        setStart(false);
-        setCountDown(countTime);
-        fn && fn();
-        return;
-      }
-      setCountDown(countDown - 1);
-    },
-    start ? defaultDelay : null,
-  );
+  useInterval(() => setCountDown((t: number) => t - 1), isStart ? defaultDelay : null);
 
-  return [countDown, start, setStart];
+  return [countDown, isStart, setStart];
 };
+
+
 export default useCountDown;
